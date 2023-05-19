1f434c3fa915b40e51f8d9a74d2780c60ae3bcc1
RachiRaw <131473716+RachiRaw@users.noreply.github.com>
2023-05-19 15:07:39 +0530

    Made and implemented comparison function for computer and user choice

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
+let userWin = 0;
+let compWin = 0;
 function getComputerChoice()
 {
     let comp = ["rock", "paper" , "scissor"];
     return comp[Math.floor(Math.random()*comp.length)];
 }
 
-console.log(getComputerChoice());
+function play(playerChoice , computerChoice)
+{
+    if((playerChoice === "rock" && computerChoice === "paper") || (playerChoice === "paper" && computerChoice === "scissor") || (playerChoice === "scissor" && computerChoice === "rock"))
+    {
+        console.log(" You lose! " + computerChoice + " beat " + playerChoice);
+        compWin++;
+    }
+    else if (playerChoice === computerChoice)
+    {
+        console.log("A draw! You both have " + playerChoice);
+    }
+
+    else
+    {
+        console.log("You win! " + playerChoice + " beat " + computerChoice);
+        userWin++;
+    }
+}
