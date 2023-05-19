fd977171fd70f24114007aaf5109d1c2de1d9e2b
RachiRaw <131473716+RachiRaw@users.noreply.github.com>
2023-05-19 15:38:50 +0530

    Made and implemented game function, debugged the win increment.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let userWin = 0;
 let compWin = 0;
+let str;
 function getComputerChoice()
 {
     let comp = ["rock", "paper" , "scissor"];
@@ -10,17 +11,46 @@ function play(playerChoice , computerChoice)
 {
     if((playerChoice === "rock" && computerChoice === "paper") || (playerChoice === "paper" && computerChoice === "scissor") || (playerChoice === "scissor" && computerChoice === "rock"))
     {
-        console.log(" You lose! " + computerChoice + " beat " + playerChoice);
-        compWin++;
+        compWin++
+        return (" You lose! " + computerChoice + " beat " + playerChoice);
     }
     else if (playerChoice === computerChoice)
     {
-        console.log("A draw! You both have " + playerChoice);
+        return ("A draw! You both have " + playerChoice);
     }
 
-    else
+    else if ((computerChoice === "rock" && playerChoice === "paper") || (computerChoice === "paper" && playerChoice === "scissor") || (computerChoice=== "scissor" && playerChoice === "rock"))
     {
-        console.log("You win! " + playerChoice + " beat " + computerChoice);
         userWin++;
+        return ("You win! " + playerChoice + " beat " + computerChoice);
+    }
+
+    else{
+        compWin++;
+        return ("Invalid hand!. Turn forfeited.!");
     }
 }
+
+function game() 
+{
+    for( let i =1 ; i<=5 ; i++)
+    {
+      str = prompt("Enter a hand, rock , paper or scissors?");
+      console.log(play(str.toLowerCase(), getComputerChoice()));  
+    }
+
+    if(userWin > compWin)
+    {
+        console.log("You won the game!!");
+    }
+    else if (compWin < userWin)
+    {
+        console.log("Oh no! You lost the game.");
+    }
+    else
+    {
+        console.log("It's a draw!!");
+    }
+}
+
+game();
